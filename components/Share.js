1e0acf8388db689a39d5c244f082ea402b1676f4
Baghdadi <b.baghdadi93@gmail.com>
2022-02-17 00:42:20 +0100

    add(Share.js) add process to create social content

--- components/Share.js
+++ components/Share.js
@@ -1,6 +1,40 @@
 import React from 'react'
 import { View, Text, Image, StyleSheet } from 'react-native'
 import { SocialIcon, Button } from 'react-native-elements';
+import {
+  shareOnFacebook,
+  shareOnTwitter,
+} from 'react-native-social-share';
+
+const tweet = () => {
+
+  shareOnTwitter({
+      'text':'J\'ai pris une photo sur #cerealis #coloring #AR !',
+      'link':'http://www.cerealis.com/',
+      //'imagelink':'https://ionnews.mu/wp-content/uploads/2022/02/kylian-mbappe.jpg',
+      //or use image
+      'image': 'logo.PNG',
+    },
+    (results) => {
+      console.log(results);
+    }
+  );
+};
+
+const facebookShare = () => {
+
+  shareOnFacebook({
+      'text':'J\'ai pris une photo sur #cerealis #coloring #AR !',
+      'link':'http://www.cerealis.com/',
+      'imagelink':'https://artboost.com/apple-touch-icon-144x144.png',
+      //or use image
+      'image': 'logo.PNG',
+    },
+    (results) => {
+      console.log(results);
+    }
+  );
+};
 
 const Share = ({navigation}) => {
   return (
@@ -14,15 +48,18 @@ const Share = ({navigation}) => {
         title='Partager avec Facebook'
         button
         type='facebook'
+        onPress={facebookShare}
       />
       <SocialIcon
         title='Partager avec Twitter'
         button
         type='twitter'
+        onPress={tweet}
       />
       <SocialIcon
         title='Partager avec Instagram'
         button
+        light
         type='instagram'
       />
       <Button title="Prendre une nouvelle photo" buttonStyle={{ backgroundColor: 'black',  borderRadius: 30, margin: 7, height: 60 }} />
