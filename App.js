dae1805dfbf4dcb5e9a11ecc06cf729481dcbd45
Kise Shary <kicshary@gmail.com>
2022-02-21 07:24:56 +0100

    ajout viro - correction typo

--- App.js
+++ App.js
@@ -8,13 +8,14 @@
 
 import React from 'react';
 import type {Node} from 'react';
-import { StyleSheet,} from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Share from './components/Share'
-import index from './components/index'
-import formulaire from './components/form'
-import react from './components/react'
+import {StyleSheet} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Share from './components/Share';
+import index from './components/index';
+import formulaire from './components/form';
+import react from './components/react';
+import photo from './components/photo';
 
 const Stack = createNativeStackNavigator();
 
@@ -22,25 +23,25 @@ const App: () => Node = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-      <Stack.Screen
+        <Stack.Screen
           name="Accueil"
           component={index}
-          options={{ title: 'Accueil' }}
+          options={{title: 'Accueil'}}
         />
         <Stack.Screen
           name="Share"
           component={Share}
-          options={{ title: 'Partager' }}
+          options={{title: 'Partager'}}
         />
         <Stack.Screen
           name="formulaire"
           component={formulaire}
-          options={{ title: 'Formulaire' }}
+          options={{title: 'Formulaire'}}
         />
         <Stack.Screen
           name="react"
           component={react}
-          options={{ title: 'React' }}
+          options={{title: 'React'}}
         />
       </Stack.Navigator>
     </NavigationContainer>
--- components/Share.js
+++ components/Share.js
@@ -1,38 +1,35 @@
-import React from 'react'
-import { View, Text, Image, StyleSheet } from 'react-native'
-import { SocialIcon, Button } from 'react-native-elements';
-import {
-  shareOnFacebook,
-  shareOnTwitter,
-} from 'react-native-social-share';
+import React from 'react';
+import {View, Text, Image, StyleSheet} from 'react-native';
+import {SocialIcon, Button} from 'react-native-elements';
+import {shareOnFacebook, shareOnTwitter} from 'react-native-social-share';
 
 const tweet = () => {
-
-  shareOnTwitter({
-      'text':'J\'ai pris une photo sur #cerealis #coloring #AR !',
-      'link':'http://www.cerealis.com/',
+  shareOnTwitter(
+    {
+      text: "J'ai pris une photo sur #cerealis #coloring #AR !",
+      link: 'http://www.cerealis.com/',
       //'imagelink':'https://ionnews.mu/wp-content/uploads/2022/02/kylian-mbappe.jpg',
       //or use image
-      'image': 'logo.PNG',
+      image: 'logo.PNG',
     },
-    (results) => {
+    results => {
       console.log(results);
-    }
+    },
   );
 };
 
 const facebookShare = () => {
-
-  shareOnFacebook({
-      'text':'J\'ai pris une photo sur #cerealis #coloring #AR !',
-      'link':'http://www.cerealis.com/',
-      'imagelink':'https://artboost.com/apple-touch-icon-144x144.png',
+  shareOnFacebook(
+    {
+      text: "J'ai pris une photo sur #cerealis #coloring #AR !",
+      link: 'http://www.cerealis.com/',
+      imagelink: 'https://artboost.com/apple-touch-icon-144x144.png',
       //or use image
-      'image': 'logo.PNG',
+      image: 'logo.PNG',
     },
-    (results) => {
+    results => {
       console.log(results);
-    }
+    },
   );
 };
 
@@ -40,40 +37,54 @@ const Share = ({navigation}) => {
   return (
     <View>
       <Text>Partager votre photo avec vos amis !</Text>
-      <Image
-          style={styles.logo}
-          source={{ uri: 'assets:/logo.PNG' }}
-        />
+      <Image style={styles.logo} source={{uri: 'assets:/logo.PNG'}} />
       <SocialIcon
-        title='Partager avec Facebook'
+        title="Partager avec Facebook"
         button
-        type='facebook'
+        type="facebook"
         onPress={facebookShare}
       />
       <SocialIcon
-        title='Partager avec Twitter'
+        title="Partager avec Twitter"
         button
-        type='twitter'
+        type="twitter"
         onPress={tweet}
       />
       <SocialIcon
-        title='Partager avec Instagram'
+        title="Partager avec Instagram"
         button
         light
-        type='instagram'
+        type="instagram"
       />
-      <Button title="Prendre une nouvelle photo" buttonStyle={{ backgroundColor: 'black',  borderRadius: 30, margin: 7, height: 60 }} />
-      <Button onPress={()=> navigation.navigate('Accueil')} title="Revenir à l'accueil" buttonStyle={{ backgroundColor: 'black',  borderRadius: 30, margin: 7, height: 60 }} />
+      <Button
+        title="Prendre une nouvelle photo"
+        buttonStyle={{
+          backgroundColor: 'black',
+          borderRadius: 30,
+          margin: 7,
+          height: 60,
+        }}
+      />
+      <Button
+        onPress={() => navigation.navigate('Accueil')}
+        title="Revenir à l'accueil"
+        buttonStyle={{
+          backgroundColor: 'black',
+          borderRadius: 30,
+          margin: 7,
+          height: 60,
+        }}
+      />
     </View>
-    )
-  };
+  );
+};
 
 const styles = StyleSheet.create({
   logo: {
     width: 70,
     height: 70,
     justifyContent: 'center',
-    alig
+    alignItems: 'center',
   },
 });
 
